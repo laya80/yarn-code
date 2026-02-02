0ded005bcb27db62afcdd1455fffe58b49dd35dc
Aliah Cristine Ariola <aliahcristineariola@gmail.com>
2026-02-02 20:52:48 +0800

    Add files via upload

---
+++ yarn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float yarn;
+    float hour;
+    float rate;
+    float price;
+    cout << "How much yarn did I spent?";
+    cin >> yarn;
+    cout << "How many hours did I work?";
+    cin >> hour;
+    cout << "What is my hourly rate?";
+    cin >> rate;
+    price = yarn + (hour * rate);
+    cout << "The final price is: " << price << endl;
+    return 0;
+}
